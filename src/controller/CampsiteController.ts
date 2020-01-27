32579a65ff0f0a8c98121c3d45272e5b0b9bd13c
Jorge Flores <jflores@elementum.com>
2020-01-27 15:13:49 -0300

    renaming

--- src/controller/CampsiteController.ts
+++ src/controller/CampsiteController.ts
@@ -1,31 +1,31 @@
 import { Request, Response } from "express";
 import {Response as ReservationResponse} from './http/response/Response';
-import * as helpers from './http/HttpHelper';
+import * as httpHelper from './http/HttpHelper';
 import {AvailabilityParams} from "./http/request/AvailabilityParams";
 
 export const availability = async (req : Request, res : Response) => {
    let params: AvailabilityParams = new AvailabilityParams(req.query.startDate,req.query.endDate);
-   let response = await helpers.GET('/reservation/availability',params);
+   let response = await httpHelper.GET('/reservation/availability',params);
   onResponse(response,res);
 };
 
 export const createReservation = async (req:Request,res:Response) =>{
-  let response = await helpers.POST('/reservation',null,req.body);
+  let response = await httpHelper.POST('/reservation',null,req.body);
   onResponse(response,res);
 };
 
 export const getReservation = async (req:Request,res:Response) =>{
-  let response = await helpers.GET(`/reservation/${req.params.id}`,null);
+  let response = await httpHelper.GET(`/reservation/${req.params.id}`,null);
   onResponse(response,res);
 };
 
 export const deleteReservation = async (req:Request,res:Response) =>{
-  let response = await helpers.DELETE(`/reservation/${req.params.id}`,req.query);
+  let response = await httpHelper.DELETE(`/reservation/${req.params.id}`,req.query);
   onResponse(response,res);
 };
 
 export const updateReservation = async(req:Request,res:Response) =>{
-  let response = await helpers.PUT(`/reservation/${req.params.id}`,req.body);
+  let response = await httpHelper.PUT(`/reservation/${req.params.id}`,req.body);
   onResponse(response,res);
 };
 
